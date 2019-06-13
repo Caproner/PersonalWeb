876c9b3e0bc77148dc7dd50d07b5988e0181282c
Caproner <634768901@qq.com>
2019-06-14 00:02:43 +0800

    写了个Hello World

---
+++ PersonalWeb/frontend/views.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+
+from django.http import HttpResponse
+
+def hello_world(request):
+    return HttpResponse('hello world')
--- PersonalWeb/mysite/urls.py
+++ PersonalWeb/mysite/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
 
 urlpatterns = [
+    url(r'', include('frontend.urls')),
     path('admin/', admin.site.urls),
 ]
