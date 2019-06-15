d8b66e43266b466cbd536b0b1a721cae38aeaaae
Caproner <634768901@qq.com>
2019-06-15 23:20:53 +0800

    fix bug

--- PersonalWeb/frontend/urls.py
+++ PersonalWeb/frontend/urls.py
@@ -4,7 +4,7 @@ from .views import IndexView, ArkDrawView
 
 
 urlpatterns = [
-    url(r'^$', IndexView.as_view(), name="index"),
+    url(r'^(|index.html)$', IndexView.as_view(), name="index"),
     url(r'^ArkNights/draw.html$', ArkDrawView.as_view(), name="ark_draw"),
     #url(r'^(?P<id>\d+)/(?P<docs>[a-zA-Z\u4E00-\u9FA5]*)$', HelloWorldView.as_view(), name="hello_world")
 ]
