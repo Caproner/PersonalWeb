0e7fe1fa73cd8f9cfb4301a3187c83a476d38837
Caproner <634768901@qq.com>
2019-06-17 01:31:47 +0800

    尝试激活按钮

--- PersonalWeb/frontend/views.py
+++ PersonalWeb/frontend/views.py
@@ -11,27 +11,11 @@ class IndexView(View):
 
 class ArkDrawView(View):
     def get(self, request, *args, **kwargs):
-
-        '''
-        agent_list = [
-            {'name' : '炎熔', 'job' : '术师', 'rank' : 3, 'star' : '★★★'},
-            {'name' : '炎熔', 'job' : '术师', 'rank' : 3, 'star' : '★★★'},
-            {'name' : '炎熔', 'job' : '术师', 'rank' : 3, 'star' : '★★★'},
-            {'name' : '炎熔', 'job' : '术师', 'rank' : 3, 'star' : '★★★'},
-            {'name' : '炎熔', 'job' : '术师', 'rank' : 3, 'star' : '★★★'},
-            {'name' : '炎熔', 'job' : '术师', 'rank' : 3, 'star' : '★★★'},
-            {'name' : '炎熔', 'job' : '术师', 'rank' : 3, 'star' : '★★★'},
-            {'name' : '炎熔', 'job' : '术师', 'rank' : 3, 'star' : '★★★'},
-            {'name' : '炎熔', 'job' : '术师', 'rank' : 3, 'star' : '★★★'},
-            {'name' : '炎熔', 'job' : '术师', 'rank' : 3, 'star' : '★★★'}
-        ]
-
-        agent_list = [
-            {'name' : '能天使', 'job' : '狙击', 'rank' : 6, 'star' : get_star(6)},
-            {'name' : '天火', 'job' : '术师', 'rank' : 5, 'star' : get_star(5)},
-            {'name' : '玫兰莎', 'job' : '近卫', 'rank' : 3, 'star' : get_star(3)},
-            {'name' : '红豆', 'job' : '先锋', 'rank' : 4, 'star' : get_star(4)}
-            ]
-        '''
+        if request.method == 'GET':
+            times = request.GET.get('times', default='0')
+        if times > 10:
+            times = 10
+        elif not times == 0:
+            times = 1
         agent_list = get_agent_list(10)
         return render(request, "ArkNights/draw.html", {'agent_list': agent_list})
