562e4e7082ed64cb14df290d3103a1d196f7fedf
Caproner <634768901@qq.com>
2019-06-16 23:12:57 +0800

    fix bug

--- PersonalWeb/frontend/model.py
+++ PersonalWeb/frontend/model.py
@@ -2,11 +2,11 @@
 from django.db import models
 
 
-class Meta:
-    verbose_name = u"干员信息表"
-    verbose_name_plural = verbose_name
-
 class AgentInfoModel(models.Model):
     name = models.CharField(max_length = 20, verbose_name = u"姓名")
     job = models.CharField(max_length = 20, verbose_name = u"职业")
     rank = models.IntegerField(default = 0, null = False, verbose_name = u"等级")
+
+    class Meta:
+        verbose_name = u"干员信息表"
+        verbose_name_plural = verbose_name
