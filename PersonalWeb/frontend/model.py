22680c8bca31ef81a815eb3a49146c83529de8b2
Caproner <634768901@qq.com>
2019-06-16 23:07:07 +0800

    优化admin前端

--- PersonalWeb/frontend/model.py
+++ PersonalWeb/frontend/model.py
@@ -3,6 +3,8 @@ from django.db import models
 
 
 class AgentInfoModel(models.Model):
+    verbose_name = u"干员信息表"
+    verbose_name_plural = verbose_name
     name = models.CharField(max_length = 20, verbose_name = u"姓名")
     job = models.CharField(max_length = 20, verbose_name = u"职业")
     rank = models.IntegerField(default = 0, null = False, verbose_name = u"等级")
