02d490380623057628390f4149e6b3d56b45e722
Caproner <634768901@qq.com>
2019-06-16 14:38:23 +0800

    fix bug

--- PersonalWeb/frontend/views.py
+++ PersonalWeb/frontend/views.py
@@ -11,9 +11,9 @@ class IndexView(View):
 class ArkDrawView(View):
     def get(self, request, *args, **kwargs):
         agent_list = [
-            {'name' = '能天使', 'job' = '狙击', 'rank' = 6},
-            {'name' = '天火', 'job' = '术师', 'rank' = 5},
-            {'name' = '玫兰莎', 'job' = '近卫', 'rank' = 3},
-            {'name' = '红豆', 'job' = '先锋', 'rank' = 4}
+            {'name' : '能天使', 'job' : '狙击', 'rank' : 6},
+            {'name' : '天火', 'job' : '术师', 'rank' : 5},
+            {'name' : '玫兰莎', 'job' : '近卫', 'rank' : 3},
+            {'name' : '红豆', 'job' : '先锋', 'rank' : 4}
             ]
         return render(request, "ArkNights/draw.html", {'agent_list': agent_list})
