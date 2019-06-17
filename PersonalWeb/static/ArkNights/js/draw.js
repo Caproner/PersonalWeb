a92adfd4c3b37e01b61db74caa5d23c9da9b906f
Caproner <634768901@qq.com>
2019-06-17 21:09:49 +0800

    fix bug

--- PersonalWeb/static/ArkNights/js/draw.js
+++ PersonalWeb/static/ArkNights/js/draw.js
@@ -4,4 +4,9 @@ $(document).ready(function(){
             $("#js-draw-show").html(data);
         });
     });
+    $("#draw-10").click(function(){
+        $.get("/ArkNights/draw.html?times=10", function(data, status){
+            $("#js-draw-show").html(data);
+        });
+    });
 });
