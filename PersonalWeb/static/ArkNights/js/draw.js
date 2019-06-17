b13f207df8371d8d3bc39bb9dcd320fc3909a85b
Caproner <634768901@qq.com>
2019-06-17 21:34:58 +0800

    尝试添加新功能的大体布局

--- PersonalWeb/static/ArkNights/js/draw.js
+++ PersonalWeb/static/ArkNights/js/draw.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
-    $("#draw-1").click(function(){
+    $("#js-draw-1").click(function(){
         $.get("/ArkNights/draw.html?times=1", function(data, status){
             $("#js-draw-show").html(data);
         });
     });
-    $("#draw-10").click(function(){
+    $("#js-draw-10").click(function(){
         $.get("/ArkNights/draw.html?times=10", function(data, status){
             $("#js-draw-show").html(data);
         });
