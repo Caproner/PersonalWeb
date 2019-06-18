e98cd31227d0edc4ee184160c3be043e0165b02f
Caproner <634768901@qq.com>
2019-06-18 15:46:58 +0800

    在admin上用户model

--- PersonalWeb/frontend/admin.py
+++ PersonalWeb/frontend/admin.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
-from .model import AgentInfoModel
+from .model import AgentInfoModel, UserInfoModel
 
+
+@admin.register(UserInfoModel)
+class UserInfoAdmin(admin.ModelAdmin):
+    list_display = ['name', 'password', 'email', 'sex', 'c_time']
+    list_per_page = 25
+    ordering = ['id', 'name', 'password', 'email', 'sex', 'c_time']
 
 @admin.register(AgentInfoModel)
 class AgentInfoAdmin(admin.ModelAdmin):
